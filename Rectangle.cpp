08affd94bd1b20956bd116532b58ec0e1f0b6f33
AnkitaMane30 <maneankita831@gmail.com>
2025-02-03 02:11:40 -0800

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Rectangle{
+	private:
+		int hei;
+		int wid;
+		int area;
+	public:
+		Rectangle(int h,int w)
+		{
+			hei=h;
+			wid=w;
+			area=hei*wid;
+			
+			cout<<"Area of Rectangle:"<<area<<endl;
+		}
+				
+};
+int main()
+{
+	int a,b;
+	cout<<"Enter height and width of Rectangle:"<<endl;
+	cin>>a>>b;
+	
+	Rectangle r1(a,b);
+	return 0;
+}
